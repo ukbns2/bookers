03f7f1d4e3d3f41d965999d73392da60740cf569
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-08-29 06:20:13 +0000

    [Add]RSpecによるテスト環境の設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -40,6 +40,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] ="Book was successfully destroyed."
     redirect_to '/books'
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -19,11 +19,4 @@ ActiveRecord::Schema.define(version: 2022_08_21_063601) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "lists", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
